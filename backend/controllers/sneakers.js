44566bd1dda77e182e1c66982d5c0bc246e22321
HelloArthur <arthurCheng0425@gmail.com>
2021-05-16 19:48:55 +0800

    Update api get sneaker by id

--- backend/controllers/sneakers.js
+++ backend/controllers/sneakers.js
@@ -4,10 +4,10 @@ export const getSneakers = async (req, res) => {
         pool.getConnection((err, connection) => {
             if (err) throw err;
             console.log(`connected as id ${connection.threadId}`)
-            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*'+
-                                'FROM brands, sneakers, sneaker_qty '+
-                                'WHERE brands.brand_id = sneakers.sneaker_brand '+
-                                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id', 
+            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*' +
+                'FROM brands, sneakers, sneaker_qty ' +
+                'WHERE brands.brand_id = sneakers.sneaker_brand ' +
+                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id',
                 (err, rows) => {
                     connection.release();
                     if (!err) {
@@ -30,12 +30,12 @@ export const getSneakersMostPopular = async (req, res) => {
         pool.getConnection((err, connection) => {
             if (err) throw err;
             console.log(`connected as id ${connection.threadId}`)
-            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*'+
-                                'FROM brands, sneakers, sneaker_qty '+
-                                'WHERE brands.brand_id = sneakers.sneaker_brand '+
-                                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id '+
-                                'ORDER BY sneakers.visit_count DESC '+
-                                'LIMIT 10', 
+            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*' +
+                'FROM brands, sneakers, sneaker_qty ' +
+                'WHERE brands.brand_id = sneakers.sneaker_brand ' +
+                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id ' +
+                'ORDER BY sneakers.visit_count DESC ' +
+                'LIMIT 10',
                 (err, rows) => {
                     connection.release();
                     if (!err) {
@@ -58,12 +58,12 @@ export const getSneakersHighest = async (req, res) => {
         pool.getConnection((err, connection) => {
             if (err) throw err;
             console.log(`connected as id ${connection.threadId}`)
-            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*'+
-                                'FROM brands, sneakers, sneaker_qty '+
-                                'WHERE brands.brand_id = sneakers.sneaker_brand '+
-                                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id '+
-                                'ORDER BY sneakers.sell DESC '+
-                                'LIMIT 5', 
+            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*' +
+                'FROM brands, sneakers, sneaker_qty ' +
+                'WHERE brands.brand_id = sneakers.sneaker_brand ' +
+                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id ' +
+                'ORDER BY sneakers.sell DESC ' +
+                'LIMIT 5',
                 (err, rows) => {
                     connection.release();
                     if (!err) {
@@ -86,12 +86,40 @@ export const getSneakersRecommended = async (req, res) => {
         pool.getConnection((err, connection) => {
             if (err) throw err;
             console.log(`connected as id ${connection.threadId}`)
-            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*'+
-                                'FROM brands, sneakers, sneaker_qty '+
-                                'WHERE brands.brand_id = sneakers.sneaker_brand '+
-                                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id '+
-                                'AND brands.brand_name = "ADIDAS"', 
+            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*' +
+                'FROM brands, sneakers, sneaker_qty ' +
+                'WHERE brands.brand_id = sneakers.sneaker_brand ' +
+                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id ' +
+                'AND brands.brand_name = "ADIDAS"',
                 (err, rows) => {
+                    connection.release();
+                    if (!err) {
+                        res.status(200).json(rows);
+                    } else {
+                        console.log(err);
+                    }
+                })
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
+
+export const getSneakersById = async (req, res) => {
+    try {
+        const sneakerId = req.params.id
+        const pool = req.pool
+        pool.getConnection((err, connection) => {
+            if (err) throw err;
+            console.log(`connected as id ${connection.threadId}`)
+            connection.query('SELECT brands.brand_name, sneakers.sneaker_name, sneakers.visit_count, sneakers.image_url, sneaker_qty.*' +
+                'FROM brands, sneakers, sneaker_qty ' +
+                'WHERE brands.brand_id = sneakers.sneaker_brand ' +
+                'AND sneakers.sneaker_id = sneaker_qty.sneaker_id ' +
+                'AND sneakers.sneaker_id = ?',
+                [sneakerId], (err, rows) => {
                     connection.release();
                     if (!err) {
                         res.status(200).json(rows);
--- backend/routes/sneakers.js
+++ backend/routes/sneakers.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getSneakers, getSneakersMostPopular, getSneakersHighest, getSneakersRecommended } from '../controllers/sneakers.js';
+import { getSneakers, getSneakersMostPopular, getSneakersHighest, getSneakersRecommended, getSneakersById } from '../controllers/sneakers.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get('/all', getSneakers);
 router.get('/mostPopular', getSneakersMostPopular);
 router.get('/highestSelling', getSneakersHighest);
 router.get('/recommended', getSneakersRecommended);
+router.get('/:id', getSneakersById);
 
 export default router;
 
